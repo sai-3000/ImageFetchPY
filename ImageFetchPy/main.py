ed68e6750924cc55104227d5447276260383abc1
sai-3000 <smaran06ncn@gmail.com>
2024-03-24 01:09:43 +0530

    Added features to save downloaded image urls in a text file

--- ImageFetchPy/main.py
+++ ImageFetchPy/main.py
@@ -1,6 +1,7 @@
 """
 Version 1.2 of ImageFetchPy
 --Includes modifications so that it does not downloads images of icons, navbar etc.
+--Now saves image url in a text file inside a folder 'url' present inside the folder    which contains images based on the given query name
 
 Author - Sai Smaran Panda
 
@@ -125,7 +126,7 @@ def download_images_from_html(html_content, directory, keyword, extensions, limi
         except Exception as e:
             pass
 
-def download_query(keywords, limit, extensions={'.jpg', '.png', '.ico', '.gif', '.jpeg'}):
+'''def download_query(keywords, limit, extensions={'.jpg', '.png', '.ico', '.gif', '.jpeg'}):
     """
     Download images based on user-provided keywords and limits.
 
@@ -152,5 +153,98 @@ def download_query(keywords, limit, extensions={'.jpg', '.png', '.ico', '.gif', 
         download_images_from_html(html_content, os.path.join(main_directory, keyword.replace(" ", "_")), keyword, extensions, limit, last_number, bar)
 
     bar.finish()
+    print("Downloading Complete")'''
+
+
+import os
+from urllib.parse import quote
+
+def create_url_directory(directory, name):
+    """
+    Create a directory to save URL text files based on the provided directory path and name.
+
+    :param directory: The main directory path where subdirectories will be created.
+    :param name: The name of the subdirectory to be created.
+    """
+    try:
+        name = name.replace(" ", "_")
+        url_directory = os.path.join(directory, name, "url")
+        if not os.path.exists(url_directory):
+            os.makedirs(url_directory)
+    except OSError as e:
+        if e.errno != 17:  # Ignore "File exists" error
+            raise
+
+def write_urls_to_file(directory, keyword, url):
+    """
+    Write the URL to a text file inside the URL directory.
+
+    :param directory: The main directory path where subdirectories are located.
+    :param keyword: The keyword associated with the URL.
+    :param url: The URL to write to the file.
+    """
+    try:
+        name = keyword.replace(" ", "_")
+        url_directory = os.path.join(directory, name, "url")
+        url_file_path = os.path.join(url_directory, f"{name}_urls.txt")
+        with open(url_file_path, 'a') as url_file:
+            url_file.write(url + '\n')
+    except Exception as e:
+        print(e)
+        raise
+
+def download_query(keywords, limit, extensions={'.jpg', '.png', '.ico', '.gif', '.jpeg'}):
+    """
+    Download images based on user-provided keywords and limits.
+
+    :param keywords: Keywords to search for images (comma-separated if multiple).
+    :param limit: Maximum number of images to download per keyword.
+    :param extensions: Set of valid image file extensions to download.
+    """
+    keyword_to_search = [str(item).strip() for item in keywords.split(',')]
+    main_directory = "images/"
+
+    for keyword in keyword_to_search:
+        create_image_directories(main_directory, keyword)
+        create_url_directory(main_directory, keyword)
+        url = f'https://www.google.com/search?q={quote(keyword.encode("utf-8"))}&tbm=isch'
+        html_content = download_page(url)
+
+        existing_images = os.listdir(os.path.join(main_directory, keyword.replace(" ", "_")))
+        existing_numbers = [int(img.split("_")[-1].split(".")[0]) for img in existing_images if img.startswith(keyword)]
+        last_number = max(existing_numbers) if existing_numbers else 0
+
+        bar = progressbar.ProgressBar(maxval=limit, widgets=[progressbar.Bar('*', '[', ']'), ' ', progressbar.Percentage()])
+        bar.start()
+
+        image_counter = 0
+        end_object = -1
+
+        while image_counter < limit:
+            try:
+                new_line = html_content.find('"https://', end_object + 1)
+                end_object = html_content.find('"', new_line + 1)
+
+                buffor = html_content.find('\\', new_line + 1, end_object)
+                if buffor != -1:
+                    object_raw = html_content[new_line + 1:buffor]
+                else:
+                    object_raw = html_content[new_line + 1:end_object]
+
+                if any(extension in object_raw for extension in extensions) and not any(pattern in object_raw for pattern in ['ssl.gstatic.com', 'www.gstatic.com']):
+                    time.sleep(1)
+                    download_image(object_raw, os.path.join(main_directory, keyword.replace(" ", "_")), keyword, extensions, last_number, bar)
+                    write_urls_to_file(main_directory, keyword, object_raw)
+                    image_counter += 1
+                    last_number += 1
+
+            except Exception as e:
+                pass
+
+        bar.finish()
+
     print("Downloading Complete")
+
+
+download_query("india",10)
 
